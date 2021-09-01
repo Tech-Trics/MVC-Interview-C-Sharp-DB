8738a711d7589da30344d3ed4074a3abe5761a36
Jeffrey Lara <jefflara0@gmail.com>
2021-09-01 09:59:44 -0500

    formatting

--- MVC-Interview-C-Sharp/Startup.cs
+++ MVC-Interview-C-Sharp/Startup.cs
@@ -28,10 +28,11 @@ namespace MVC_Interview_C_Sharp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+                .AddEntityFrameworkStores<ApplicationDbContext>();            
+
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
@@ -63,6 +64,7 @@ namespace MVC_Interview_C_Sharp
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
                 endpoints.MapRazorPages();
             });
         }
